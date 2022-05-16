6c3527fd488ec8e6bc06b4b5e23e199b381316d2
Fernando Favini <darthjee@gmail.com>
2022-05-17 00:37:47 +0200

    Category controller (#8)

---
+++ source/app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  resource_for Move::Category,
+               only: :index,
+               paginated: true,
+               per_page: Settings.pagination
+end
---
+++ source/spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe CategoriesController, type: :controller do
+  let(:expected_json) do
+    Move::Category::Decorator.new(expected_object).to_json
+  end
+
+  describe 'GET index' do
+    let(:categories_count) { 1 }
+    let(:parameters) { {} }
+
+    render_views
+
+    before do
+      create_list(:move_category, categories_count)
+    end
+
+    context 'when requesting json', :not_cached do
+      let(:expected_object) { Move::Category.all }
+
+      before do
+        get :index, params: parameters.merge(format: :json)
+      end
+
+      it { expect(response).to be_successful }
+
+      it 'returns categories serialized' do
+        expect(response.body).to eq(expected_json)
+      end
+
+      it 'adds page header' do
+        expect(response.headers['page']).to eq(1)
+      end
+
+      it 'adds pages header' do
+        expect(response.headers['pages']).to eq(1)
+      end
+
+      it 'adds per_page header' do
+        expect(response.headers['per_page']).to eq(Settings.pagination)
+      end
+
+      context 'when there are too many categories' do
+        let(:categories_count) { 2 * Settings.pagination + 1 }
+        let(:expected_object) { Move::Category.all.limit(Settings.pagination) }
+
+        it { expect(response).to be_successful }
+
+        it 'returns categories serialized' do
+          expect(response.body).to eq(expected_json)
+        end
+
+        it 'adds page header' do
+          expect(response.headers['page']).to eq(1)
+        end
+
+        it 'adds pages header' do
+          expect(response.headers['pages']).to eq(3)
+        end
+
+        it 'adds per_page header' do
+          expect(response.headers['per_page']).to eq(Settings.pagination)
+        end
+      end
+
+      context 'when requesting last page' do
+        let(:categories_count) { 2 * Settings.pagination + 1 }
+        let(:expected_object) do
+          Move::Category.all.offset(2 * Settings.pagination)
+        end
+        let(:parameters) { { page: 3 } }
+
+        it { expect(response).to be_successful }
+
+        it 'returns categories serialized' do
+          expect(response.body).to eq(expected_json)
+        end
+
+        it 'adds page header' do
+          expect(response.headers['page']).to eq(3)
+        end
+
+        it 'adds pages header' do
+          expect(response.headers['pages']).to eq(3)
+        end
+
+        it 'adds per_page header' do
+          expect(response.headers['per_page']).to eq(Settings.pagination)
+        end
+      end
+    end
+  end
+end

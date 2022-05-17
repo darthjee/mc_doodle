aa28bbe938e0a9e92b94b4e4a1f35e13a9bcc880
darthjee <darthjee@gmail.com>
2022-05-18 00:40:32 +0200

    Transform category id into string

--- source/app/assets/javascripts/controllers/move/item/controllers.js
+++ source/app/assets/javascripts/controllers/move/item/controllers.js
@@ -1,7 +1,7 @@
 (function(angular, _) {
   var module = angular.module('move/item/controllers', ['cyberhawk']);
 
-  var Methods = {
+  var withCategories = {
     requestCategories: function() {
       this.requester.http.get('/categories')
         .then(this._setCategories)
@@ -9,15 +9,24 @@
 
     _setCategories: function(response) {
       this.categories = response.data;
+    },
+
+    _transformData: function() {
+      if (!this.data.category_id) {
+        return
+      }
+
+      this.data.category_id = this.data.category_id.toString();
     }
   };
 
   module.config(['cyberhawkProvider', function(provider) {
     var Controller = provider.$get().controller,
-      paths = ['/moves/:move_id/items/new', '/moves/:move_id/items/:id/edit'];
+      formPaths = ['/moves/:move_id/items/new', '/moves/:move_id/items/:id/edit'];
 
-    Controller.withPath(paths, Methods);
+    Controller.withPath(formPaths, withCategories);
 
-    Controller.on(paths, 'request', 'requestCategories');
+    Controller.on(formPaths, 'request', 'requestCategories');
+    Controller.on(formPaths, 'loaded', '_transformData');
   }]);
 }(window.angular, window._));

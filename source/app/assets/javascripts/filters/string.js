fcd0e6b3c8b930a23b5a01ff8ec7ec0e8496eb7f
darthjee <darthjee@gmail.com>
2022-05-18 00:03:20 +0200

    Remove old filters

--- source/app/assets/javascripts/filters/string.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-  angular
-    .module("mc_doodle")
-    .filter("string", function() {
-      return function(input) { 
-        return input.toString();
-      };
-    });
-})(window.angular);

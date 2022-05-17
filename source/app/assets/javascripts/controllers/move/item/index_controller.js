86286bfb473fda553ff4cb08c6d180259eaba8a1
darthjee <darthjee@gmail.com>
2022-05-17 19:08:13 +0200

    Use new controller configuration

--- source/app/assets/javascripts/controllers/move/item/index_controller.js
+++ source/app/assets/javascripts/controllers/move/item/index_controller.js
@@ -1,4 +1,22 @@
 (function(angular, _) {
   var module = angular.module('move/item/index_controller', ['cyberhawk']);
-}(window.angular, window._));
 
+  var Methods = {
+    requestCategories: function() {
+      var that = this;
+      this.requester.http.get('/categories')
+        .then(function(response) {
+          that.categories = response.data;
+        });
+    }
+  };
+
+  module.config(['cyberhawkProvider', function(provider) {
+    var Controller = provider.$get().controller,
+      path = '/moves/:move_id/items/new';
+
+    Controller.withPath(path, Methods);
+
+    Controller.on(path, 'request', 'requestCategories');
+  }]);
+}(window.angular, window._));
